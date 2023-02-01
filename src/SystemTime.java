5fcc33a91fe429f851626d0a019cc4351432ed5f
Hugo Chagas <hugochagasuk@gmail.com>
2023-02-01 16:41:15 +0000

    add method main to 1. Print today's date and time, 2. Print OS name, 3. Print screen size, 4. Print display language

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class SystemTime {
+import java.awt.*;
+import java.util.*;
+
+public class SystemTime {
+    public static void main(String[] args) {
+
+        /*Print today's date and time*/
+        Date date = new Date();
+        System.out.println("The time is" + " " + date);
+
+        /*Print OS name*/
+        System.out.println("My OS is" + " " + System.getProperty("os.name"));
+
+        /*Print Screen size*/
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int)size.getWidth();
+        int height = (int)size.getHeight();
+
+        /*Print my display language*/
+        Locale currentLocale = Locale.getDefault();
+        System.out.println("My display idiom is " + currentLocale.getDisplayLanguage());
+
+
+        System.out.println("My screen resolution is " + width + " x " + height);
+
+    }
+
+
 }
+
